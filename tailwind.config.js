297f9ee15dace6fe744ad11392e20e2d6f756817
Srijan Pokhrel <srijan.pokhrel113@gmail.com>
2020-07-24 01:22:02 +0545

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    backgroundColor:['responsive','hover','focus','active']
+  },//hover active focus responsive are varient
+  plugins: [],
+}
+
+// how to enable active class in tailwind
